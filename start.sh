24566ddaaea0c7127f5082f398efae1ec8361c88
khb.hnu <khb.hnu@gmail.com>
2023-04-21 20:53:20 +0800

    start.sh

--- start.sh
+++ start.sh
@@ -3,11 +3,17 @@
 home=$(cd $(dirname $0);pwd)
 cd ${home}
 
-running=$(ps ax | grep "xiaogpt.py" | grep "xiao_config.json" | grep -v grep | wc -l)
+[[ $# -ne 2 ]] && { echo "please input config file"; exit; }
+
+config="$1"
+
+exec="xiaogpt.py"
+
+running=$(ps -ef | grep -w "${exec}" | grep -w "${config}" | grep -v grep | wc -l)
 if [[ ${running} -ge 1 ]];then
 	echo "$(date) program is running, running_num:${running}"
-	if [[ "$1" == "kill" || ${running} -gt 1 ]];then
-		ps -ef | grep -w "xiaogpt.py" | grep "xiao_config.json" | grep -v grep  | gawk '{print $2}' | while read pid;do
+	if [[ "$2" == "restart" || ${running} -gt 1 ]];then
+		ps -ef | grep -w "${exec}" | grep -w "${config}" | grep -v grep  | gawk '{print $2}' | while read pid;do
 			kill -9 $pid
 		done
 	else
@@ -16,4 +22,4 @@ if [[ ${running} -ge 1 ]];then
 fi
 
 
-nohup python3 xiaogpt.py --config xiao_config.json >> data.log &
+nohup python3 ${exec} --config ${config} >> log_${config} &
